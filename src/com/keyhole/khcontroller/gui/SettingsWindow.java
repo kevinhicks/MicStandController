0858f8589d79976d66f0f67e85a07080b599dea9
kevinhicks <keyholesoftware@gmail.com>
2014-11-09 23:20:23 -0500

    Implemented Settings

--- src/com/keyhole/khcontroller/gui/SettingsWindow.java
+++ src/com/keyhole/khcontroller/gui/SettingsWindow.java
@@ -23,118 +23,129 @@ import com.keyhole.khcontroller.util.Settings;
 
 @SuppressWarnings("serial")
 public class SettingsWindow extends Frame implements WindowListener {
-	
+
 	private TextField txtIp;
 	private TextField txtPort;
-	
+
 	private TextField txtRaiseMicRelay;
 	private TextField txtLowerMicRelay;
 	private TextField txtRaiseScreenRelay;
 	private TextField txtLowerScreenRelay;
 	private Checkbox chkShowScreenControls;
-	
+
 	private Button btnCancel;
 	private Button btnSave;
-	
+
 	private SettingsWindow() {
-		setSize(300,300);
+		setSize(300, 300);
 		setVisible(true);
 		addWindowListener(this);
 		setLayout(new GridLayout(0, 2));
-		
+
 		txtIp = new TextField(Settings.GetIp());
 		txtPort = new TextField(Settings.GetPort() + "");
 
-		txtLowerMicRelay = new TextField(Settings.GetLowerMicRelay());
-		txtRaiseMicRelay = new TextField(Settings.GetRaiseMicRelay());
-		txtLowerScreenRelay = new TextField(Settings.GetLowerScreenRelay());
-		txtRaiseScreenRelay = new TextField(Settings.GetRaiseScreenRelay());
-		chkShowScreenControls = new Checkbox("", Settings.GetShowScreenControls());
-		
+		txtLowerMicRelay = new TextField(Settings.GetLowerMicRelay() + "");
+		txtRaiseMicRelay = new TextField(Settings.GetRaiseMicRelay() + "");
+		txtLowerScreenRelay = new TextField(Settings.GetLowerScreenRelay() + "");
+		txtRaiseScreenRelay = new TextField(Settings.GetRaiseScreenRelay() + "");
+		chkShowScreenControls = new Checkbox("",
+				Settings.GetShowScreenControls());
+
 		btnCancel = new Button("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+
+			}
+		});
+
 		btnSave = new Button("Save");
 		btnSave.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Settings.SetIp(txtIp.getText());
 				Settings.SetIp(txtPort.getText());
 				Settings.SaveSettings();
+
+				setVisible(false);
 			}
 		});
-		
+
 		add(new Label("IP Address:"));
 		add(txtIp);
-		
+
 		add(new Label("Port:"));
 		add(txtPort);
-		
+
 		add(new Label("Raise Mic Relay:"));
 		add(txtRaiseMicRelay);
-		
+
 		add(new Label("Lower Mic Relay:"));
 		add(txtLowerMicRelay);
-		
+
 		add(new Label("Raise Screen Relay:"));
 		add(txtRaiseScreenRelay);
-		
+
 		add(new Label("Lower Screen Relay:"));
 		add(txtLowerScreenRelay);
-		
+
 		add(new Label("Show Screen Controls:"));
-		add(chkShowScreenControls);		
-		
+		add(chkShowScreenControls);
+
 		add(btnCancel);
 		add(btnSave);
 	}
-	
+
 	private static SettingsWindow _instance = null;
-	public static SettingsWindow GetInstance() 
-	{
-		if(_instance == null) 
-		{
+
+	public static SettingsWindow GetInstance() {
+		if (_instance == null) {
 			_instance = new SettingsWindow();
-		}					
+		}
 		return _instance;
 	}
-	
+
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0) {
-		setVisible(false);		
+		setVisible(false);
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
-		setVisible(false);		
+		setVisible(false);
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 };
--- src/com/keyhole/khcontroller/util/Settings.java
+++ src/com/keyhole/khcontroller/util/Settings.java
@@ -1,123 +1,177 @@
 package com.keyhole.khcontroller.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Settings {
-	
+
 	static {
 		new Settings();
 	}
-	
-	private int lowerMicRelay;
-	private int raiseMicRelay;
-	private int lowerScreenRelay;
-	private int raiseScreenRelay;
-	
-	private String ip;
-	private int port;
-	
-	private boolean showScreenControls;
-	
+
+	private static int lowerMicRelay;
+	private static int raiseMicRelay;
+	private static int lowerScreenRelay;
+	private static int raiseScreenRelay;
+
+	private static String ip;
+	private static int port;
+
+	private static boolean showScreenControls;
+
+	private static Properties properties;
+
 	private Settings() {
 
-		Properties prop = new Properties();
-		
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+		properties = new Properties();
+
+		// Get the default settings
 		try {
-			prop.load(inputStream);
+			properties.putAll(GetDefaultSettings());
 		} catch (IOException e) {
-			System.out.println("exceptop");
+			System.out.println("Default Settings Not Found");
 			return;
 		}
-		if (inputStream == null) {
-			System.out.println("error");
+
+		// Get the user specific settings
+		try {
+			properties.putAll(GetUserSettings());
+		} catch (IOException e) {
+			System.out.println("User Settings Not Found");
 			return;
 		}
- 
-		ip = prop.getProperty("ip");
-		//TODO check for missing config file & invalid settings
-		port = Integer.parseInt(prop.getProperty("port"));
-		raiseMicRelay = Integer.parseInt(prop.getProperty("RaiseMicRelay"));
-		lowerMicRelay = Integer.parseInt(prop.getProperty("LowerMicRelay"));
-		raiseScreenRelay = Integer.parseInt(prop.getProperty("RaiseScreenRelay"));
-		lowerScreenRelay = Integer.parseInt(prop.getProperty("LowerScreenRelay"));
 	}
-	
-	private void GetDefaultSettings() {
+
+	private Properties GetDefaultSettings() throws IOException {
+		Properties props = new Properties();
+
+		props.load(ClassLoader.getSystemResourceAsStream("config.properties"));
+		return GetSettings(props);
+	}
+
+	private Properties GetUserSettings() throws IOException {
+		Properties props = new Properties();
+
+		// Load form the user dir
+		String dir = System.getProperty("user.home");
+		InputStream inStream = new FileInputStream(dir
+				+ "/Mic Stand Controller/config.properties");
+		props.load(inStream);
+		inStream.close();
+
+		return GetSettings(props);
+	}
+
+	public Properties GetSettings(Properties props) {
+		ip = props.getProperty("ip");
 		
-	}
-	
-	private void GetUserSettings() {
+		try {
+			port = Integer.parseInt(props.getProperty("port"));
+		} catch (NumberFormatException nfe) {
+
+		}
+
+		try {
+			raiseMicRelay = Integer.parseInt(props.getProperty("RaiseMicRelay"));
+		} catch (NumberFormatException nfe) {
+
+		}
 		
+		try {
+			lowerMicRelay = Integer.parseInt(props.getProperty("LowerMicRelay"));
+		} catch (NumberFormatException nfe) {
+
+		}
+		
+		try {
+			raiseScreenRelay = Integer.parseInt(props.getProperty("RaiseScreenRelay"));
+		} catch (NumberFormatException nfe) {
+
+		}
+		
+		try {
+			lowerScreenRelay = Integer.parseInt(props.getProperty("LowerScreenRelay"));
+		} catch (NumberFormatException nfe) {
+
+		}
+
+		return props;
 	}
-	
+
 	public static void SaveSettings() {
-		
-	}
-	
-	public static int GetLowerMicRelay()
-	{
-		return 2;
+		File userHome = new File(System.getProperty("user.home")
+				+ "/Mic Stand Controller/");
+		if (!userHome.isDirectory()) {
+			userHome.mkdirs();
+		}
+
+		File propertiesFile = new File(userHome, "config.properties");
+
+		try {
+			properties.store(new FileOutputStream(propertiesFile), "");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	public static void SetLowerMicRelay(int relay)
-	{
-		
+	public static int GetLowerMicRelay() {
+		return lowerMicRelay;
 	}
 
-	public static int GetRaiseMicRelay()
-	{
-		return 1;
+	public static void SetLowerMicRelay(int relay) {
+		lowerMicRelay = relay;
 	}
 
-	public static void SetRaiseMicRelay(int relay)
-	{
-		
-	}
-	
-	public static int GetLowerScreenRelay()
-	{
-		return 4;
+	public static int GetRaiseMicRelay() {
+		return raiseMicRelay;
 	}
 
-	public static void SetLowerScreenRelay(int relay)
-	{
-		
+	public static void SetRaiseMicRelay(int relay) {
+		raiseMicRelay = relay;
 	}
 
-	public static int GetRaiseScreenRelay()
-	{
-		return 3;
+	public static int GetLowerScreenRelay() {
+		return lowerScreenRelay;
 	}
 
-	public static void SetRaiseScreenRelay(int relay)
-	{
-		
+	public static void SetLowerScreenRelay(int relay) {
+		lowerScreenRelay = relay;
 	}
-	
+
+	public static int GetRaiseScreenRelay() {
+		return raiseScreenRelay;
+	}
+
+	public static void SetRaiseScreenRelay(int relay) {
+		raiseScreenRelay = relay;
+	}
+
 	public static String GetIp() {
-		return "192.168.0.123";
+		return ip;
 	}
-	
-	public static void SetIp(String ip) {
-		
+
+	public static void SetIp(String ipAddress) {
+		ip = ipAddress;
 	}
-	
+
 	public static int GetPort() {
-		return 80;
+		return port;
 	}
-	
-	public static void SetPort(int port) {
-		
+
+	public static void SetPort(int portNumber) {
+		port = portNumber;
 	}
-	
+
 	public static boolean GetShowScreenControls() {
-		return true;
+		return showScreenControls;
 	}
-	
-	public static void SetShowScreenControls() {
-		
+
+	public static void SetShowScreenControls(boolean showScreenControls) {
+		Settings.showScreenControls = showScreenControls;
 	}
 }
